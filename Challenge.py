134127019f795d8058e80700ca6b9cf21ecd63ba
Ethan Tsang <ethan.tsang@Y22-Design-Ethan-Tsang.local>
2018-10-24 13:52:43 -0400

    sdjfksajdkfjasklfjklasdjf

---
+++ Challenge.py
@@ -0,0 +1,13 @@
+num = input("Type a number: ")
+num = int(num)
+
+if (num > 100):
+	print("It is bigger")
+    
+elif (100 > num):
+    print("It is smaller")
+    
+elif (100 == num):
+    print("They are equal")
+
+#chained conditional ^^^^
